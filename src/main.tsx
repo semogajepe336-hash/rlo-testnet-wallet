93c29c0e539382c7d3656766c0d4d14408c49efc
semogajepe336-hash <330087968+semogajepe336-hash@users.noreply.github.com>
2026-09-23 18:39:04 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1640,7 +1640,7 @@ setVaultUnlocked(true);}catch{setStatus("Incorrect password. Please try again.")
     </div>
   }
 
-  <footer>©2026 Rialo Wallet. ·</footer>
+  <footer>©2026 Rialo Wallet. </footer>
  </main>
 </>
 }
